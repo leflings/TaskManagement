efbcfe43ba451aa173c7c9876e07b11745dd10d9
Jesper Plantener <jesper@plantener.dk>
2012-10-29 20:51:47 +0100

    Opdateret SelectUser og utils.

--- src/Views/SelectUser.java
+++ src/Views/SelectUser.java
@@ -10,6 +10,7 @@ import DataTransferObjects.User;
 public class SelectUser implements BaseView {
 
 	private List<User> users;
+	private int choice;
 
 	public SelectUser(List<User> users){
 		this.users = users;
@@ -21,15 +22,15 @@ public class SelectUser implements BaseView {
 		for (int i = 0 ; i < users.size(); i++) {
 			SelectUtilities.printSelectLine(i+1,users.get(i).getUserName() + " (id: " + users.get(i).getUserId() + ")");		
 		}
-		
+		choice = SelectUtilities.selectChoice() - 1;
 		
 	}
 
 	
 	public User getResult(){
-		//TODO
+		User selectedUser = users.get(choice);
 		
-		return null;
+		return selectedUser;
 		
 	}
 	
--- src/utilities/SelectUtilities.java
+++ src/utilities/SelectUtilities.java
@@ -1,13 +1,19 @@
 package utilities;
 
+import java.util.Scanner;
+
 public class SelectUtilities {
 
 	public static void printSelectLine(int number, String text){
 		System.out.format("[%-2d] : %s%n", number, text);
 	}
 	
-	public static void selectChoice(){
+	public static int selectChoice(){
+		Scanner scan = new Scanner(System.in);
+		System.out.format("Vælg fra ovenstående liste");
+		int choice = scan.nextInt();
 		
+		return choice;
 	}
 	
 }
