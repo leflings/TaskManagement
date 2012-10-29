2f54211f63bc54672ce051a6446f2d81c0080902
Jesper Plantener <jesper@plantener.dk>
2012-10-29 16:57:12 +0100

    Utility class tilføjet m.m.

--- src/Views/SelectUser.java
+++ src/Views/SelectUser.java
@@ -1,11 +1,35 @@
 package Views;
 
+import java.util.List;
+
+import utilities.SelectUtilities;
+
+import DataTransferObjects.User;
+
 
 public class SelectUser implements BaseView {
 
+	private List<User> users;
+
+	public SelectUser(List<User> users){
+		this.users = users;
+	}
+	
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
+		
+		for (int i = 0 ; i < users.size(); i++) {
+			SelectUtilities.printSelectLine(i+1,users.get(i).getUserName() + " (id: " + users.get(i).getUserId() + ")");		
+		}
+		
+		
+	}
+
+	
+	public User getResult(){
+		//TODO
+		
+		return null;
 		
 	}
 	
---
+++ src/utilities/SelectUtilities.java
@@ -0,0 +1,13 @@
+package utilities;
+
+public class SelectUtilities {
+
+	public static void printSelectLine(int number, String text){
+		System.out.format("[%-2d] : %s%n", number, text);
+	}
+	
+	public static void selectChoice(){
+		
+	}
+	
+}
