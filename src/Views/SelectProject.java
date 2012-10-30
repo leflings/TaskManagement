a3ce436feca0de665f831aeda927a402fdbc11ad
Jesper Plantener <jesper@plantener.dk>
2012-10-30 17:52:45 +0100

    Flere opdaterede views

--- src/Views/SelectProject.java
+++ src/Views/SelectProject.java
@@ -1,11 +1,32 @@
 package Views;
 
+import java.util.List;
+
+import utilities.SelectUtilities;
+
+import DataTransferObjects.Project;
+
 
 public class SelectProject implements BaseView{
 
+	private List<Project> projects;
+	private int choice;
+
+	public SelectProject(List<Project> projects){
+		this.projects = projects;
+	}
+	
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
+		for (int i = 0 ; i < projects.size(); i++) {
+			SelectUtilities.printSelectLine(i+1,projects.get(i).getProjectName() + " (id: " + projects.get(i).getProjectId() + ")");		
+		}
+		choice = SelectUtilities.selectChoice();
 		
+	}
+	
+	public Project getResult(){
+		Project selectedProject = projects.get(choice - 1);
+		return selectedProject;
 	}
 }
--- src/Views/SelectUser.java
+++ src/Views/SelectUser.java
@@ -22,13 +22,13 @@ public class SelectUser implements BaseView {
 		for (int i = 0 ; i < users.size(); i++) {
 			SelectUtilities.printSelectLine(i+1,users.get(i).getUserName() + " (id: " + users.get(i).getUserId() + ")");		
 		}
-		choice = SelectUtilities.selectChoice() - 1;
+		choice = SelectUtilities.selectChoice();
 		
 	}
 
 	
 	public User getResult(){
-		User selectedUser = users.get(choice);
+		User selectedUser = users.get(choice - 1); //Trækker en fra for at hente den rigtige user, da vi lægger én til i tidl.
 		
 		return selectedUser;
 		
