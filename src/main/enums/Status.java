e548609e9d73ec493111e6ac599d193c1a75ca23
Flemming Madsen <mail@leflings.dk>
2012-11-22 12:50:38 +0100

    Adds SelectEnum and updates enum types to implement IEnum

--- src/main/enums/Status.java
+++ src/main/enums/Status.java
@@ -1,17 +1,23 @@
 package main.enums;
 
-public enum Status {
+public enum Status implements IEnum {
 	
-	NONE(0), ACCEPTED(1), IN_PROGRESS(2), FINISHED(3), DECLINED(4);
+	NONE(0, "Ingen"), ACCEPTED(1, "Accepteret"), IN_PROGRESS(2, "Undervejs"), FINISHED(3, "Færdig"), DECLINED(4, "Afvist");
 	
 	private int code;
+	private String text;
 	
-	private Status(int i) {
+	private Status(int i, String text) {
 		code = i;
+		this.text = text;
 	}
 	
 	public int getCode() {
 		return code;
+	}
+	
+	public String toString() {
+		return text;
 	}
 	
 	public Status fromCode(int code) {
